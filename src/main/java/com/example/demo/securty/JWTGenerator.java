60b49d49c18dd94184cf5401648f3379fac639ba
senunn” <senuxxx123@gmail.com>
2024-06-25 23:23:49 +0530

    JWT Generator done

--- src/main/java/com/example/demo/securty/JWTGenerator.java
+++ src/main/java/com/example/demo/securty/JWTGenerator.java
@@ -1,10 +1,14 @@
 package com.example.demo.securty;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.util.Date
+import java.util.Date;
 
 import static com.example.demo.securty.SecurityConstants.JWTexpiration;
 
@@ -17,7 +21,30 @@ public class JWTGenerator {
         Date expireDate = new Date(currentDate.getTime() + JWTexpiration);
 
 
-//        String token = jwt.builder
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.ES512, SecurityConstants.JWTsecret)
+                .compact();
+        return token;
+    }
+
+    public String getusernamefromjwt (String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWTsecret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(SecurityConstants.JWTsecret).parseClaimsJws(token);
+            return true;
+        }catch (Exception ex){
+            throw new AuthenticationCredentialsNotFoundException("JWT was incorrect or expired");
+        }
     }
 
 }
--- src/main/java/com/example/demo/securty/SecurityConstants.java
+++ src/main/java/com/example/demo/securty/SecurityConstants.java
@@ -2,4 +2,5 @@ package com.example.demo.securty;
 
 public class SecurityConstants {
     public static final long JWTexpiration = 7000;
+    public static final String JWTsecret = "secret";
 }
